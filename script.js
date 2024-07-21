e30b764115d85640567fd8cd78e3a53561fbc936
inaldomacena <inaldo446@hotmail.com>
2024-07-21 02:51:41 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav-menu');
+
+    menuToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
+});
